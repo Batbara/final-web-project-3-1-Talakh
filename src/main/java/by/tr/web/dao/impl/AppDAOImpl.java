6e1eba90a988ee60e9953bc9faf4e3046ee87d40
OSmol <Olga_Smolyakova@outlook.com>
2017-12-06 15:08:10 +0300

    Update AppDAOImpl.java

--- src/main/java/by/tr/web/dao/impl/AppDAOImpl.java
+++ src/main/java/by/tr/web/dao/impl/AppDAOImpl.java
@@ -5,7 +5,10 @@ import by.tr.web.dao.impl.connection_pool.ConnectionPool;
 import by.tr.web.exception.dao.ApplicationDAOException;
 import by.tr.web.exception.dao.ConnectionPoolException;
 
-public class AppDAOImpl implements AppDAO {
+public class AppDAOImpl implements AppDAO {// интерфейс AppDao не относится ко всему DAO
+    // он определяет только наличие connection pool, что адекватно не при всех реализациях
+    // поэму расположение этого интерфейчас в корне слой не совсем обосновано
+    // плюс название AppDAO - это трэш
     @Override
     public void initConnectionPool() throws ApplicationDAOException {
         ConnectionPool connectionPool = ConnectionPool.getInstance();
@@ -13,6 +16,7 @@ public class AppDAOImpl implements AppDAO {
             connectionPool.initPoolData();
         } catch (ConnectionPoolException e) {
             throw new ApplicationDAOException("Failed to init connection pool", e);
+            // ApplicationDAOException - тоже трэшовое какое-то название
         }
     }
 }
