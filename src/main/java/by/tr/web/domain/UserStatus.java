22a9205e9aad76b1928e4375da968b582154d87f
OSmol <Olga_Smolyakova@outlook.com>
2017-12-11 12:59:31 +0300

    Update UserStatus.java

--- src/main/java/by/tr/web/domain/UserStatus.java
+++ src/main/java/by/tr/web/domain/UserStatus.java
@@ -1,5 +1,6 @@
 package by.tr.web.domain;
 
 public enum UserStatus {// не уверена, что этот класс должен находиться в пакете domain
+    // возможно его следует определить вложенным классом
     ADMIN, USER, GUEST
 }
