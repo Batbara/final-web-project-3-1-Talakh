111f2d22e03f3ad388f435ccd6f0b2aa4be87e92
OSmol <Olga_Smolyakova@outlook.com>
2017-12-11 13:01:27 +0300

    Update AppDAO.java

--- src/main/java/by/tr/web/dao/AppDAO.java
+++ src/main/java/by/tr/web/dao/AppDAO.java
@@ -2,6 +2,6 @@ package by.tr.web.dao;
 
 import by.tr.web.exception.dao.ApplicationDAOException;
 
-public interface AppDAO {
+public interface AppDAO {// что это за странное и некорректное название интерфейса и исключения - как по названию интерфейса догадаться для чего он предназначен?
     void initConnectionPool() throws ApplicationDAOException;
 }
